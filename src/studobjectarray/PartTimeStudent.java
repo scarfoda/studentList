0c7c36873ced1d7a579ab2139b90061c3e857d64
scarfoda <scarfoda@sheridancollege.ca>
2021-06-02 12:38:44 -0400

    Brach1 added PartTimeStudent class

---
+++ src/studobjectarray/PartTimeStudent.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studobjectarray;
+
+/**
+ *
+ * @author darre
+ */
+public class PartTimeStudent extends Student {
+
+    private int numOfCourses;
+    
+    /**
+     * @return the numOfCourses
+     */
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+
+    /**
+     * @param numOfCourses the numOfCourses to set
+     */
+    public void setNumOfCourses(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+    
+    
+    
+    
+    
+}
